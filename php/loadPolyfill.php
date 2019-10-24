3aef97ce4246b430c4370a89bfc86567f7160bc4
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2019-10-24 08:59:04 -0400

    Creates initial modx script
    Copied an example from
    `https://docs.modx.com/revolution/2.x/developing-in-modx/advanced-development/modx-services/modfilehandler`
    
    - makes a js file
    - checks if the file actually is created
      - logs modx error if unsuccessful
    - puts the contents of the polyfill from cdnjs
    - returns a success message

---
+++ php/loadPolyfill.php
@@ -0,0 +1,10 @@
+<?php
+$modx->getService('fileHandler','modFileHandler');
+$file = $modx->fileHandler->make('/assests/js/polyfill/');
+if (!$file->create('cssrelpreload.min.js')) {
+   return $modx->log(modX::LOG_LEVEL_DEBUG, 'loadPolyfill was unable to create file preload.js');
+}
+
+file_put_contents($file->getPath(), 'https://cdnjs.cloudflare.com/ajax/libs/loadCSS/2.1.0/cssrelpreload.min.js');
+
+return $modx->log(modX::LOG_LEVEL_DEBUG, 'loadPolyfill created file preload.js');
